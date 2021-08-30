e9388503443b729b52427ef34e66d6db94444299
muyildirim <musyil@netrd.com.tr>
2021-08-30 19:49:41 +0300

    initial structure of prototype is created

---
+++ src/main/java/com/neueda/atm/VerySimpleATM.java
@@ -0,0 +1,13 @@
+package com.neueda.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerySimpleATM {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerySimpleATM.class, args);
+    }
+
+}
---
+++ src/test/java/com/neueda/atm/VerySimpleATMTests.java
@@ -0,0 +1,13 @@
+package com.neueda.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerySimpleATMTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
